245d9c058e74ba623d28993a75cd4595b3582b1a
Gavin Wilson <gmanwilson@icloud.com>
2024-12-31 15:08:20 -0500

    Added additional styling through JS when adding li

--- app.js
+++ app.js
@@ -11,9 +11,21 @@ button.addEventListener('click', () => {
     if (task_text !== '') {
         const new_li = document.createElement('li');
         new_li.innerText = task_text;
+        new_li.style.display = 'flex'
+        new_li.style.backgroundColor = '#cfbaf0'
+        new_li.style.fontSize = '1.25em';
+        new_li.style.paddingLeft = '.5em'
+        new_li.style.alignItems = 'center'
+        new_li.style.justifyContent = 'space-between'
+        new_li.style.borderRadius = '5px'
+        new_li.style.border = '1px solid #8e89f1'
 
         const delete_button = document.createElement('button');
-        delete_button.innerText = 'Delete';
+        delete_button.innerText = 'X';
+        delete_button.style.backgroundColor = '#cfbaf0';
+        delete_button.style.border = '1.5px solid #8e89f1'
+        delete_button.style.borderRadius = '5px';
+        delete_button.style.cursor = 'pointer';
         delete_button.addEventListener('click', () => {
             new_li.remove();
         });
